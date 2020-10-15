8ddd7c0b58e66469ee78dd3c93dc285bd366f74c
Josh Kuss <joshua.d.kuss@gmail.com>
2020-10-15 17:18:27 -0500

    Create client.js

---
+++ client.js
@@ -0,0 +1,8 @@
+// ## Array Cardio Day 2
+
+    const people = [
+      { name: 'Wes', year: 1988 },
+      { name: 'Kait', year: 1986 },
+      { name: 'Irv', year: 1970 },
+      { name: 'Lux', year: 2015 }
+    ];
